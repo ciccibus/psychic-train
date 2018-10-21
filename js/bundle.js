5004fe57f60777d176d85cc5b29fa92ed42db6d0
ciccibus <domsmasher@live.com>
2018-10-21 13:14:05 +0100

    musical fiesta

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,41 @@
 (function () {
 	'use strict';
 
+	function render() {
+	  console.log("render!");
+	}
+
+	function unwrapExports (x) {
+		return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
+	}
+
+	function createCommonjsModule(fn, module) {
+		return module = { exports: {} }, fn(module, module.exports), module.exports;
+	}
+
+	var dist = createCommonjsModule(function (module, exports) {
+	Object.defineProperty(exports,"__esModule",{value:!0});var logger=function(e,t){console.group("Logger"),console.log("State:",JSON.stringify(e,null,2)),console.log("New State",JSON.stringify(t,null,2)),console.groupEnd();},observable=function(e,n){return new Proxy(e,{set:function(t,e,r){var o=Object.assign({},t);return Reflect.set(t,e,r),n.forEach(function(e){if("function"!=typeof e)throw new Error("Listener must be a function!");e(o,t);}),{name:e,value:r}}})},store=function(){for(var e=0<arguments.length&&void 0!==arguments[0]?arguments[0]:{},t=arguments.length,r=new Array(1<t?t-1:0),o=1;o<t;o++)r[o-1]=arguments[o];var n=observable(e,r);return {getStore:function(){return Object.assign({},n)},setStore:function(t){return Object.keys(t).forEach(function(e){Reflect.set(n,e,t[e]);}),Object.assign({},n,t)}}};exports.logger=logger,exports.createStore=store;
+
+	});
+
+	unwrapExports(dist);
+	var dist_1 = dist.logger;
+	var dist_2 = dist.createStore;
+
+	var INITIAL_STATE = {
+	  message: ""
+	};
+	var state = dist_2(INITIAL_STATE, dist_1);
+	state.setStore({
+	  message: "Yay! is working!",
+	  action: "Ready!"
+	});
+	var html = "<pre>".concat(JSON.stringify(state.getStore(), null, 2), "</pre>");
+	document.body.innerHTML = html;
+	console.log("=============");
+	console.log(JSON.stringify(state.getStore(), null, 2));
+	console.log("=============");
+	render();
 	console.log("yay!");
 
 }());
--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 "use strict"
 
+import { render } from "./render";
+import { logger, createStore } from 'musical-fiesta';
+
+const INITIAL_STATE = { message: ""};
+
+const state = createStore(INITIAL_STATE, logger);
+
+state.setStore({
+	message: "Yay! is working!",
+	action: "Ready!"
+});
+
+const html = `<pre>${JSON.stringify(state.getStore(), null, 2)}</pre>`;
+
+document.body.innerHTML = html;
+console.log("=============");
+console.log(JSON.stringify(state.getStore(), null, 2));
+console.log("=============");
+
+render();
 console.log("yay!");
